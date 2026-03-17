a841009377c302c90ca42455aeed798181bae139
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-17 21:51:22 +0530

    Modified bucket attribute

--- terraform/live/dev/main.tf
+++ terraform/live/dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "mys3" {
-    bucket_name = "terraform-cicd"
+    bucket = "terraform-cicd"
 }
 
 resource "aws_s3_bucket_public_access_block" "buckpolicy" {
