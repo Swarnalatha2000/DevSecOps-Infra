775fb566215741ef98024f595d590007dabde244
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-17 21:39:08 +0530

    Added s3 code and workflow for developer flow

---
+++ terraform/live/dev/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "mys3" {
+    bucket_name = "terraform-cicd"
+}
+
+resource "aws_s3_bucket_public_access_block" "buckpolicy" {
+  bucket = aws_s3_bucket.mys3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
