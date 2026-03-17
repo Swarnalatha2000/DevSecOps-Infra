21fec6bb66b030c9aa9ee2a2f604c5e76bf763a3
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-17 21:55:00 +0530

    Formatted the code

--- terraform/live/dev/main.tf
+++ terraform/live/dev/main.tf
@@ -7,10 +7,9 @@ resource "aws_s3_bucket" "mys3" {
 }
 
 resource "aws_s3_bucket_public_access_block" "buckpolicy" {
-  bucket = aws_s3_bucket.mys3.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+    bucket = aws_s3_bucket.mys3.id
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
 }
