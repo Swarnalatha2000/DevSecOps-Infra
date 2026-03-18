df4116044a1605bd01fa13cacc4dd0b01c24b5d6
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-18 09:51:47 +0530

    Working on checkov issues Enabled the versioning and lifecycle configuration

--- terraform/live/dev/main.tf
+++ terraform/live/dev/main.tf
@@ -8,8 +8,46 @@ resource "aws_s3_bucket" "mys3" {
 
 resource "aws_s3_bucket_public_access_block" "buckpolicy" {
     bucket = aws_s3_bucket.mys3.id
+    
     block_public_acls       = true
     block_public_policy     = true
     ignore_public_acls      = true
     restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "myver" {
+  bucket = aws_s3_bucket.mys3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.myver]
+
+  bucket = aws_s3_bucket.mys3.id
+
+  rule {
+
+    filter {
+      prefix = ""
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+
+    status = "Enabled"
+  }
 }
