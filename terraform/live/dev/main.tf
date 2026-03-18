89485f56c7e6702e062fcca9eab460b1c226e5e7
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-18 17:46:14 +0530

    Working on the checkov and formatting issues

--- terraform/live/dev/main.tf
+++ terraform/live/dev/main.tf
@@ -15,8 +15,26 @@ resource "aws_s3_bucket_public_access_block" "buckpolicy" {
     restrict_public_buckets = true
 }
 
+# Log bucket and logs were enabled
+resource "aws_s3_bucket" "logs" {
+  bucket = "my-app-s3-logs"
+}
+
+resource "aws_s3_bucket_logging" "log_config" {
+  bucket        = aws_s3_bucket.mys3.id
+  target_bucket = aws_s3_bucket.logs.id
+  target_prefix = "access-logs/"
+}
+
 resource "aws_s3_bucket_versioning" "myver" {
   bucket = aws_s3_bucket.mys3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "destver" {
+  bucket = aws_s3_bucket.destination.id
   versioning_configuration {
     status = "Enabled"
   }
@@ -29,6 +47,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
   bucket = aws_s3_bucket.mys3.id
 
   rule {
+    id = "default-lifecycle-rule"
 
     filter {
       prefix = ""
@@ -49,5 +68,161 @@ resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
     }
 
     status = "Enabled"
+  }
+}
+
+# Enabling the notification event through SNS
+resource "aws_s3_bucket_notification" "sns_trigger" {
+  bucket = aws_s3_bucket.mys3.id
+
+  topic {
+    topic_arn = aws_sns_topic.topic.arn
+    events    = ["s3:ObjectRemoved:*"]
+  }
+}
+
+# Enabling the KMS encryption
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_kms_key" "dest_key" {
+  description             = "This key is used to encrypt bucket objects -destination key"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "mykms" {
+  bucket = aws_s3_bucket.mys3.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "dest_enc" {
+  bucket = aws_s3_bucket.destination.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.dest_key.arn
+    }
+  }
+}
+
+# iam role for replication
+data "aws_iam_policy_document" "replication_trust" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "replication_role" {
+  name               = "s3-replication-role"
+  assume_role_policy = data.aws_iam_policy_document.replication_trust.json
+}
+
+data "aws_iam_policy_document" "replication_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionTagging",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      aws_s3_bucket.source.arn,
+      "${aws_s3_bucket.source.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags",
+      "s3:ObjectOwnerOverrideToBucketOwner"
+    ]
+
+    resources = [
+      aws_s3_bucket.destination.arn,
+      "${aws_s3_bucket.destination.arn}/*"
+    ]
+  }
+
+  # KMS permissions for replicating encrypted objects
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = [
+      aws_kms_key.mykey.arn,
+      aws_kms_key.dest_key.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "replication_policy" {
+  name   = "S3ReplicationPolicy"
+  policy = data.aws_iam_policy_document.replication_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "replication_attach" {
+  role       = aws_iam_role.replication_role.name
+  policy_arn = aws_iam_policy.replication_policy.arn
+}
+
+# Enabling cross region replication
+
+resource "aws_s3_bucket" "destination" {
+  bucket = "terraform-destination"
+}
+
+resource "aws_s3_bucket_replication_configuration" "replication" {
+  region = "eu-central-1"
+
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.mys3]
+
+  role   = aws_iam_role.replication.arn
+  bucket = aws_s3_bucket.source.id
+
+  rule {
+    id = "examplerule"
+
+    filter {
+      prefix = "example"
+    }
+
+    status = "Enabled"
+
+    destination {
+      bucket        = aws_s3_bucket.destination.arn
+      storage_class = "STANDARD"
+    }
   }
 }
