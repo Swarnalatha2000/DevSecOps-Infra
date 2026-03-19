9a6b40ebc17b039f16d8c2b726ae1facc8323e6c
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-19 16:57:47 +0530

    Final

--- terraform/live/dev/backend.tf
+++ terraform/live/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket         = "terraform-state-swarna"
-        key            = "/dev/terraform.tfstate"
+        key            = "dev/terraform.tfstate"
         region         = "ap-south-1"
         dynamodb_table = "terraform-lock-table"
         encrypt        = true
