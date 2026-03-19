9f879fd6cd4ed96d503be31f685c14dfa43cd914
Andey Swarnalatha <swarnalatha.a2000@gmail.com>
2026-03-19 16:22:14 +0530

    Working

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_logging" "this" {
     count  = var.enable_logging && var.log_bucket != null ? 1 : 0
 
     bucket = aws_s3_bucket.this.id
-    depends_on = [aws_s3_bucket.log_bucket]
+    depends_on = [aws_s3_bucket.this]
     target_bucket = var.log_bucket
     target_prefix = "log/"
 }
